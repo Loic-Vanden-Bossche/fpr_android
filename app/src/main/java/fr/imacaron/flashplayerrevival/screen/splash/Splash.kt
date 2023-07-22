5f7227a4bb624c562223ff6f1347c11cb3cb1fad
Denis Turbiez <denisft77@gmail.com>
2023-07-23 00:53:33 +0200

    Better Splash screen

--- app/src/main/java/fr/imacaron/flashplayerrevival/screen/splash/Splash.kt
+++ app/src/main/java/fr/imacaron/flashplayerrevival/screen/splash/Splash.kt
@@ -1,9 +1,20 @@
 package fr.imacaron.flashplayerrevival.screen.splash
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import fr.imacaron.flashplayerrevival.R
 
 @Composable
 fun Splash(){
-    Text("Splash")
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Image(painterResource(R.drawable.fpr_logo_background), "Splash Logo", Modifier.size(196.dp))
+        Image(painterResource(R.drawable.fpr_logo_foreground), "Splash Logo", Modifier.size(196.dp))
+    }
 }
